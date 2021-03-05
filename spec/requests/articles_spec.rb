cb341e0abfde02bb3e55f6ba07494a8d795e4e54
alejoboga <alejoboga20@gmail.com>
2021-03-05 16:11:55 -0300

    Adding index controller action

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -3,8 +3,25 @@ require 'rails_helper'
 RSpec.describe '/articles routes' do
 	describe '#index' do
 		it 'returns a success response' do
-			get '/index'
+			get '/articles'
 			expect(response).to have_http_status(:ok)
+		end
+
+		it 'returns a proper JSON' do
+			article = create :article
+			get '/articles'
+
+			aggregate_failures do
+				expect(json_data.length).to eq(1)
+				expected = json_data.first
+				expect(expected[:id]).to eq(article.id.to_s)
+				expect(expected[:type]).to eq('articles')
+				expect(expected[:attributes]).to eq(
+					title: article.title,
+					content: article.content,
+					slug: article.slug
+				)
+			end
 		end
 	end	
 end
